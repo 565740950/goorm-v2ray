a00f9a4832c3257bcb1e565acbbfb94bb9502994
小朋 <2810580330@qq.com>
2020-03-06 00:28:27 +0800

    Add files via upload

--- goormv2.sh
+++ goormv2.sh
@@ -7,7 +7,7 @@ wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ray
 #设置运行权限
 chmod +x /v2ray/v2ray
 rm -f /v2ray/v2ctl
-wget https://https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ctl
+wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ctl
 chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
 wget https://github.com/byxiaopeng/goorm-v2ray/raw/master/config.json
